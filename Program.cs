d382f0bd44afeffe458d9e2b8442a114b4623e02
Natnicha24 <natnicha.ant@mail.kmutt.ac.th>
2021-03-28 11:55:06 +0700

    Test Numbeer2

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Number2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b,x,y;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            x = a;
+            y = b;
+            while (x != y)
+            {
+                if (x < y)
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }
+            }
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
